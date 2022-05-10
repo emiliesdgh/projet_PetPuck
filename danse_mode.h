cc0993d4b8688f1b183197cbaa71ceeec64fbe05
em <emilie.grandjean@epfl.ch>
2022-05-10 15:53:32 +0200

    dance_mode pour que ca fonctionne avec le flag et  une boucle for

--- danse_mode.h
+++ danse_mode.h
@@ -8,9 +8,30 @@
 #define POSITION_REACHED        1
 
 
+#define PI                  3.1415926536f
+//TO ADJUST IF NECESSARY. NOT ALL THE E-PUCK2 HAVE EXACTLY THE SAME WHEEL DISTANCE
+#define WHEEL_DISTANCE      5.35f    //cm
+#define PERIMETER_EPUCK     (PI * WHEEL_DISTANCE)
+
+
 void motor_set_danse_speed(float speed_r, float speed_l);
 
-void dancing_puck(int8_t value);
+void motor_set_danse(float speed_r, float speed_l,float position);
+int8_t get_value_test_in_dance(void);
+
+void dancing_puck_withStep(void);
+void dancing_puck(void);
+
+void motor_set_danse_position(float position_r, float position_l, float speed_r, float speed_l);
+
+void motor_stop(void);
+
+uint8_t motor_position_reached(void);
+
+void motor_set_position(float position_r, float position_l, float speed_r, float speed_l);
+
+void motor_set_speed(float speed_r, float speed_l); // fonction qui  fait bouger le moteur
+
 
 void GoodMorning_motors(void);
 
